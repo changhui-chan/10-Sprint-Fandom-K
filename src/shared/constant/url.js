8c40f047e498288c8091ebc0949e07f2e59c6077
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-27 11:12:26 +0900

    feat: url constant 파일 추가

---
+++ src/shared/constant/url.js
@@ -0,0 +1,5 @@
+export const URL_VOTE = 'https://fandom-k-api.vercel.app/10-3/votes/';
+export const URL_IMAGE = 'https://fandom-k-api.vercel.app/images/upload/';
+export const URL_IDOLS = 'https://fandom-k-api.vercel.app/10-3/idols/';
+export const URL_DONATIONS = 'https://fandom-k-api.vercel.app/10-3/donations/';
+export const URL_CHARTS = 'https://fandom-k-api.vercel.app/10-3/charts/';
