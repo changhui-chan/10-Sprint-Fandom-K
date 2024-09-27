80f687a00ac7524669cd086a9590f421fafda50a
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-27 16:19:39 +0900

    feat: index.jsx scss 파일 기본값 추가 및 스타일링

--- src/shared/ui/modal/index.jsx
+++ src/shared/ui/modal/index.jsx
@@ -4,16 +4,29 @@ import ModalFooter from './ModalFooter';
 import Button from '../Button';
 
 const Modal = ({
+  headerText = '모달 헤더 텍스트',
+  footerElement = null,
   children = null,
+  sectionMarginTop = '24px',
+  sectionMarginBottom = '24px',
   buttonText = '확인',
   buttonClick = () => {},
 }) => {
   return (
-    <div className={styles.container}>
-      <ModalHeader />
-      {children}
-      <Button onClick={buttonClick}>{buttonText}</Button>
-      <ModalFooter />
+    <div className={styles.overlay}>
+      <div className={styles.container}>
+        <ModalHeader headerText={headerText} />
+        <div
+          style={{
+            marginTop: sectionMarginTop,
+            marginBottom: sectionMarginBottom,
+          }}
+        >
+          {children}
+        </div>
+        <Button onClick={buttonClick}>{buttonText}</Button>
+        <ModalFooter footerElement={footerElement} />
+      </div>
     </div>
   );
 };
