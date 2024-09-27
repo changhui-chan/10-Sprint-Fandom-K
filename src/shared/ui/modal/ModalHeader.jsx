2e8740e4bbda946da71a5cc7734871cc32d26f69
changhui-chan <dpfwlfhtep584@gmail.com>
2024-09-27 17:34:11 +0900

    feat: 모달 visiable 제어 기능 추가

--- src/shared/ui/modal/ModalHeader.jsx
+++ src/shared/ui/modal/ModalHeader.jsx
@@ -1,11 +1,16 @@
 import CloseIcon from '@/assets/images/btn-delete.svg';
 import styles from './ModalHeader.module.scss';
+import useModalStore from './useModalStore';
 
 const ModalHeader = ({ headerText }) => {
+  const { removeElement } = useModalStore();
+
   return (
     <div className={styles.container}>
       <p>{headerText}</p>
-      <img src={CloseIcon} alt="close" />
+      <button type="button" onClick={removeElement}>
+        <img src={CloseIcon} alt="close" />
+      </button>
     </div>
   );
 };
--- src/shared/ui/modal/index.jsx
+++ src/shared/ui/modal/index.jsx
@@ -2,6 +2,7 @@ import styles from './index.module.scss';
 import ModalHeader from './ModalHeader';
 import ModalFooter from './ModalFooter';
 import Button from '../Button';
+import useModalStore from './useModalStore';
 
 const Modal = ({
   headerText = '',
@@ -12,22 +13,28 @@ const Modal = ({
   buttonText = '확인',
   buttonClick = () => {},
 }) => {
+  const { isVisible } = useModalStore();
+
   return (
-    <div className={styles.overlay}>
-      <div className={styles.container}>
-        <ModalHeader headerText={headerText} />
-        <div
-          style={{
-            marginTop: sectionMarginTop,
-            marginBottom: sectionMarginBottom,
-          }}
-          className={styles.content}
-        >
-          {children}
+    <div>
+      {isVisible && (
+        <div className={styles.overlay}>
+          <div className={styles.container}>
+            <ModalHeader headerText={headerText} />
+            <div
+              style={{
+                marginTop: sectionMarginTop,
+                marginBottom: sectionMarginBottom,
+              }}
+              className={styles.content}
+            >
+              {children}
+            </div>
+            <Button onClick={buttonClick}>{buttonText}</Button>
+            <ModalFooter footerElement={footerElement} />
+          </div>
         </div>
-        <Button onClick={buttonClick}>{buttonText}</Button>
-        <ModalFooter footerElement={footerElement} />
-      </div>
+      )}
     </div>
   );
 };
