786502227e03cb8570edb0df66d670fb36e9b1e6
changhui-chan <dpfwlfhtep584@gmail.com>
2024-10-07 07:39:01 +0900

    feat: Header 컴포넌트 구현

---
+++ src/shared/layout/Header.jsx
@@ -0,0 +1,36 @@
+import Menu from '@/assets/images/ic-menu.svg';
+import { Link } from 'react-router-dom';
+import Logo from '../ui/Logo';
+import styles from './Header.module.scss';
+import Credit from '../ui/Credit';
+import Avatar from '../ui/Avatar';
+import Sidebar from '../ui/Sidebar';
+import { useSidebarStore } from '../ui/useSidebar';
+
+const Header = () => {
+  const { openSidebar } = useSidebarStore();
+
+  const onClick = () => {
+    openSidebar();
+  };
+
+  return (
+    <>
+      <div className={styles.container}>
+        <button type="button" onClick={onClick}>
+          <img src={Menu} alt="메뉴" className={styles.menu} />
+        </button>
+        <Link to="/support">
+          <Logo />
+        </Link>
+        <div className={styles.user}>
+          <Credit />
+          <Avatar />
+        </div>
+      </div>
+      <Sidebar />
+    </>
+  );
+};
+
+export default Header;
--- src/shared/ui/useSidebar.js
+++ src/shared/ui/useSidebar.js
@@ -1,7 +1,9 @@
 import { create } from 'zustand';
 
 export const useSidebarStore = create((set) => ({
-  isOpen: true,
+  isOpen: false,
+  openSidebar: () => set({ isOpen: true }),
+  closeSidebar: () => set({ isOpen: false }),
   toggle: () => set((state) => ({ isOpen: !state.isOpen })),
 }));
 
