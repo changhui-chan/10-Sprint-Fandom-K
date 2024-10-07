19ceca4631bb196f8ded8000b51eb8274dc7c7bf
naji <jitwona99@gmail.com>
2024-10-08 06:36:28 +0900

    feat: 크레딧 부족 알림 모달 컴포넌트 구현 (#56)
    * feat: 크레딧 부족 알림 모달 컴포넌트 구현
    
    * chore: 컴포넌트 명 및 파일명 변경

---
+++ src/features/credit/AlertModal.jsx
@@ -0,0 +1,28 @@
+import Modal from '@/shared/ui/modal';
+import creditIcon from '@/assets/images/ic-credit.svg';
+import useModalStore from '@/shared/ui/modal/useModalStore';
+import styles from './AlertModal.module.scss';
+
+const AlertModal = () => {
+  const { removeElement } = useModalStore();
+
+  return (
+    <Modal
+      customModalContainerStyle={styles.modal}
+      customModalButtonStyle={styles.button}
+      onClose={removeElement}
+      buttonClick={removeElement}
+    >
+      <div className={styles.content}>
+        <div className={styles.imageContainer}>
+          <img src={creditIcon} alt="크레딧 아이콘" />
+        </div>
+        <p>
+          앗! 투표하기 위한 <strong>크레딧</strong>이 부족해요
+        </p>
+      </div>
+    </Modal>
+  );
+};
+
+export default AlertModal;
