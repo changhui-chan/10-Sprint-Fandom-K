5cdbceeb954d5c9b267075a3e3bcd7c415ed72ba
changhui-chan <163362771+changhui-chan@users.noreply.github.com>
2024-10-10 13:31:22 +0900

    feat: 헤더 디자인 일부 변경 및 추가 기능 구현 (#75)
    * style: 헤더 내부 패딩값 조정
    
    * style: 아바타와 크레딧 간 간격 조정
    
    * feat: 마이페이지 링크 연결
    
    * feat: 사이드바 user 링크 추가 연결 및 스타일 일부 수정
    
    * fix: input radio 모양 추가 및 디자인 일부 수정
    
    * feat: 사이드바 애니메이션 및 외부 클릭했을 때 닫히는 로직 구현

--- src/shared/ui/Sidebar.jsx
+++ src/shared/ui/Sidebar.jsx
@@ -4,14 +4,18 @@ import homeIcon from '@/assets/images/ic-home.svg';
 import starIcon from '@/assets/images/ic-star.svg';
 import rankIcon from '@/assets/images/ic-rank.svg';
 import forwardIcon from '@/assets/images/ic-forward.svg';
-import { useEffect } from 'react';
+import createSupportIcon from '@/assets/images/ic-create-support.svg';
+import accountIcon from '@/assets/images/ic-account.svg';
+import { useEffect, useRef, useState } from 'react';
 import Avatar from './Avatar';
 import Logo from './Logo';
 import styles from './Sidebar.module.scss';
 import { useSidebarStore } from './useSidebar';
 
-const Sidebar = () => {
+const Sidebar = (buttonClick = () => {}) => {
   const { isOpen, closeSidebar } = useSidebarStore();
+  const [visible, setVisible] = useState(false);
+  const sidebarRef = useRef(null);
 
   useEffect(() => {
     const handleEscape = (event) => {
@@ -27,11 +31,44 @@ const Sidebar = () => {
     };
   }, [closeSidebar]);
 
+  useEffect(() => {
+    if (isOpen) {
+      setVisible(true);
+    } else {
+      setVisible(false);
+    }
+  }, [isOpen]);
+
+  const handleOutsideClick = (e) => {
+    if (sidebarRef.current && !sidebarRef.current.contains(e.target)) {
+      closeSidebar();
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      buttonClick();
+    }
+  };
+
   return (
     <div>
       {isOpen && (
-        <div className={styles.overlay}>
-          <div className={styles.container}>
+        <div
+          className={styles.overlay}
+          onClick={(e) => handleOutsideClick(e)}
+          onKeyUp={handleKeyPress}
+          role="button"
+          tabIndex={0}
+        >
+          <div
+            className={
+              visible
+                ? `${styles.container} ${styles.visible}`
+                : styles.container
+            }
+            ref={sidebarRef}
+          >
             <div className={styles.header}>
               <Logo customLogoStyle={styles.logo} />
               <button onClick={closeSidebar}>
@@ -39,8 +76,10 @@ const Sidebar = () => {
               </button>
             </div>
             <div className={styles.user}>
-              <Avatar />
-              <p>User</p>
+              <Link to="/mypage" onClick={closeSidebar}>
+                <Avatar />
+                <p>User</p>
+              </Link>
             </div>
             <div className={styles.menu}>
               <section className={styles.general}>
@@ -65,7 +104,7 @@ const Sidebar = () => {
                         className={styles.link}
                         onClick={closeSidebar}
                       >
-                        <p>후원하기</p>
+                        <p>후원 페이지</p>
                         <img src={forwardIcon} alt="화살표 아이콘" />
                       </Link>
                     </li>
@@ -76,7 +115,36 @@ const Sidebar = () => {
                         className={styles.link}
                         onClick={closeSidebar}
                       >
-                        <p>차트</p>
+                        <p>차트 페이지</p>
+                        <img src={forwardIcon} alt="화살표 아이콘" />
+                      </Link>
+                    </li>
+                  </ul>
+                </nav>
+              </section>
+              <section className={styles.account}>
+                <span className={styles.label}>USER</span>
+                <nav className={styles.nav}>
+                  <ul>
+                    <li>
+                      <img src={accountIcon} alt="어카운트 아이콘" />
+                      <Link
+                        to="/mypage"
+                        className={styles.link}
+                        onClick={closeSidebar}
+                      >
+                        <p>마이 페이지</p>
+                        <img src={forwardIcon} alt="화살표 아이콘" />
+                      </Link>
+                    </li>
+                    <li>
+                      <img src={createSupportIcon} alt="후원 아이콘" />
+                      <Link
+                        to="/mypage/create-support"
+                        className={styles.link}
+                        onClick={closeSidebar}
+                      >
+                        <p>후원 등록하기</p>
                         <img src={forwardIcon} alt="화살표 아이콘" />
                       </Link>
                     </li>
