5407695e06c61ee39ea1c92ff6bc347829fdc385
박지민 <25wlals@gmail.com>
2024-10-10 12:49:18 +0900

    fix: 폼 미작성 상태(inValid 초기값)에서 제출 가능했던 이슈 수정

--- src/shared/constant/QUESTIONS.jsx
+++ src/shared/constant/QUESTIONS.jsx
@@ -1,5 +1,5 @@
 const QUESTIONS = {
-  IDOL: '서포트 하려는 아이돌을 선택해주세요',
+  IDOLID: '서포트 하려는 아이돌을 선택해주세요',
   TITLE: '서포트의 이름을 입력해주세요',
   SUBTITLE: '서포트 설명을 입력해주세요',
   DEADLINE: '서포트 마감일을 입력해주세요',
@@ -8,13 +8,13 @@ const QUESTIONS = {
   SUBTITLE_EX: '○○역 진행예정',
   DEADLINE_EX: 'YYYY-MM-DD 00:00:00',
   TARGETDONATION_EX: '000,000',
-  IDOL_ERR: '유효한 값이 아닙니다',
+  IDOLID_ERR: '값을 비워둘 수 없습니다',
   TITLE_ERR: '값을 비워둘 수 없습니다',
   SUBTITLE_ERR: '값을 비워둘 수 없습니다',
   DEADLINE_ERR: '미래의 일시를 선택해주세요',
-  TARGETDONATION_ERR: '숫자만 입력해주세요',
+  TARGETDONATION_ERR: '유효한 숫자 값을 입력해주세요',
 };
 
-const IDOL_EX = { GENDER: '성별', GROUP: '그룹명', MEMBER: '멤버이름' };
+const IDOLID_EX = { GENDER: '성별', GROUP: '그룹명', MEMBER: '멤버이름' };
 
-export { QUESTIONS, IDOL_EX };
+export { QUESTIONS, IDOLID_EX };
