93f75bb1f6e02cdc06257d261f142257b755da2b
박지민 <25wlals@gmail.com>
2024-10-10 13:25:41 +0900

    feat: 반응형 구현

--- src/pages/createsupport/index.jsx
+++ src/pages/createsupport/index.jsx
@@ -3,11 +3,10 @@ import styles from '@/pages/createsupport/index.module.scss';
 
 const CreateSupportPage = () => {
   return (
-    <div className={styles.background}>
+    <div className={styles.container}>
       <CreateSupportForm />
     </div>
   );
 };
-// 의미x div는 <>fh 교체
 
 export default CreateSupportPage;
